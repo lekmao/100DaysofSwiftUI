f1caa5edae4823a467d00f11a7f65c9f0b08b5e2
Lékan <lekmao2004@gmail.com>
2021-03-09 22:14:43 -0600

    To start the three question challenge

--- WordScramble/WordScramble/ContentView.swift
+++ WordScramble/WordScramble/ContentView.swift
@@ -13,12 +13,34 @@ struct ContentView: View {
     @State private var rootWord: String = ""
     @State private var newWord: String = ""
     
+    
+    // alert properties
+    @State private var errorTitle = ""
+    @State private var errorMessage = ""
+    @State private var showingError = false
+
+    
     func addNewWord() {
         // lowercase and trim the charcters entered
         let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
         
         // exit method if TextField is empty
         guard answer.count > 0 else {
+            return
+        }
+        
+        guard isOriginal(word: answer) else {
+            wordError(title: "Be more original!", message: "This word has already been used!")
+            return
+        }
+        
+        guard isPossible(word: answer) else {
+            wordError(title: "Hold on there!", message: "You cannot just pull that off and pretend no one is watching!")
+            return
+        }
+        
+        guard isReal(word: answer) else {
+            wordError(title: "Really...", message: "\(answer) is not a real word!")
             return
         }
         
@@ -66,6 +88,22 @@ struct ContentView: View {
         return true
     }
     
+    // check if the word is a real word
+    func isReal(word: String) -> Bool {
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        
+        return misspelledRange.location == NSNotFound
+    }
+    
+    // setting up error alert
+    func wordError(title: String, message: String) {
+        errorTitle = title
+        errorMessage = message
+        showingError = true
+    }
+    
     var body: some View {
         NavigationView {
             VStack {
@@ -81,6 +119,9 @@ struct ContentView: View {
             }
             .navigationBarTitle(rootWord)
             .onAppear(perform: startGame)
+            .alert(isPresented: $showingError) {
+                Alert(title: Text(errorTitle), message: Text(errorMessage), dismissButton: .default(Text("Ok")))
+            }
         }
     }
 }

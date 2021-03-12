5c13bdd8678c25e5890bb5e4aa2753432d8bad19
Lékan <lekmao2004@gmail.com>
2021-03-11 23:40:43 -0600

    Start from Day 33

--- _resource/Animations/Animations/ContentView.swift
+++ _resource/Animations/Animations/ContentView.swift
@@ -8,9 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var animationAmount: Double = 0.0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Button("Tap Here") {
+            withAnimation(.interpolatingSpring(stiffness: 10, damping: 1)) {
+                self.animationAmount += 360
+            }
+        }
+        .padding(50)
+        .background(Color.red)
+        . foregroundColor(.white)
+        .clipShape(Circle())
+        .rotation3DEffect(
+            .degrees(animationAmount),
+            axis: (x: 1.0, y: 0.0, z: 0.0)
+        )
+        
     }
 }
 

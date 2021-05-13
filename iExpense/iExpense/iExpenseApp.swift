9495afed9b3a0ea186dd8da1f0bfb4708b7eb5c5
Lékan <lekmao2004@gmail.com>
2021-05-13 03:41:02 -0500

    Started Day 36

---
+++ iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Lékan Mabayoje on 5/13/21.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
